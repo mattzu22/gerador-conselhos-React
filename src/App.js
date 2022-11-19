c121e8c9c88d83e328d2cad2c44a54fac51a6601
Matheus Macedo <matheusg3.201168@gmail.com>
2022-11-18 22:25:54 -0300

    finalizado o consumo de api com js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import CSSreset from "./component/CSSreset/CSSReset";
 import GeradorDeConselhos from "./component/Main";
 
-function App() {
-  
 
+// const theme = {
+//   light: {
+//     backgroundBase: "#f9f9f9",
+//     backgroundLevel1: "#DCDCDC",
+//     backgroundLevel2: "#f0f0f0",
+//     borderBase: "#e5e5e5",
+//     textColorBase: "#222222",
+//   },
+//   dark: {
+//     backgroundBase: "#181818",
+//     backgroundLevel1: "#202020",
+//     backgroundLevel2: "#313131",
+//     borderBase: "#383838",
+//     textColorBase: "#FFFFFF",
+//   },
+// };
+
+function App() {
   return (
     <>
       <CSSreset />
--- src/component/Main/index.js
+++ src/component/Main/index.js
@@ -1,23 +1,58 @@
-import React from "react";
-import desktop from "../../images/img-desktop.svg"
-import button from "../../images/button.svg"
+import { useState, useEffect } from "react";
+import desktop from "../../images/img-desktop.svg";
+import button from "../../images/button.svg";
 import StyleGeradorDeConselhos from "./style";
 
-
 const GeradorDeConselhos = () => {
+  const [conselho, setConselho] = useState({
+    id: 0,
+    advice: "",
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const url = "https://api.adviceslip.com/advice";
+      const response = await fetch(url);
+      const json = await response.json();
+
+      setConselho({
+        id: json.slip.id,
+        advice: json.slip.advice,
+      });
+    };
+
+    fetchData();
+  }, []);
+
+  const changeAdvice = async () => {
+    const url = "https://api.adviceslip.com/advice";
+    const response = await fetch(url);
+    const json = await response.json();
+
+    setConselho({
+      id: json.slip.id,
+      advice: json.slip.advice,
+    });
+  };
+
   return (
     <StyleGeradorDeConselhos>
       <div className="gerador-conselhos">
-        <p className="conselho">ADVICE<span>#54</span></p>
-
-        <p className="mensagem">
-            "Always seek out advice or opinions when making a decision."
+        <p className="conselho">
+          ADVICE <span>#{conselho.id}</span>
         </p>
-    
+
+        <p className="mensagem">"{conselho.advice}"</p>
+
         <img src={desktop} alt="image" />
 
-        <div className="btn-back">
-          <img className="btn" src={button} alt="botão" style={{background: "green"}}/>
+        <div className="btn-back" onClick={changeAdvice}>
+          <img
+            className="btn"
+            src={button}
+            alt="botão"
+            style={{ background: "green" }}
+          />
         </div>
       </div>
     </StyleGeradorDeConselhos>
